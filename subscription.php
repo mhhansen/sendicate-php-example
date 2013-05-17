7cb898dda280e05b874ee37c28550ecd7c69df71
Martin Hansen <martin.hansen@gmail.com>
2013-05-17 08:17:58 -0300

    Changed error log file name

--- subscription.php
+++ subscription.php
@@ -10,7 +10,7 @@ ini_set('error_reporting', E_ALL);
 error_reporting(E_ALL);
 ini_set('log_errors',TRUE);
 ini_set('html_errors',FALSE);
-ini_set('error_log','error_log.txt');
+ini_set('error_log','errors.log');
 ini_set('display_errors',FALSE);
 
 include_once('lib/wrapper.php');
